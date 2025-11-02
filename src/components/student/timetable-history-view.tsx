739b8eb49b1b3996aa89b7beac0642ac270a4152
ADITYA PRASAD <babuaditya098@gmail.com>
2025-11-02 22:51:27 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/components/student/timetable-history-view.tsx
+++ src/components/student/timetable-history-view.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from "react";
 import { Badge } from "@/components/ui/badge";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
